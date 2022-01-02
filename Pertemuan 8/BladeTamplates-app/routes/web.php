f62eba61603fbd512497b2b5402a4e2dbc97679c
yolandapasaribu <yolapasaribu123@gmail.com>
2022-01-02 17:22:07 +0700

    prak 8

---
+++ Pertemuan 8/BladeTamplates-app/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/',[MyController::class,'index'])->name('index');
+Route::get('/tentang',[MyController::class,'about'])->name('about');
